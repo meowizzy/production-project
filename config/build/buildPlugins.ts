91d6448580fe0f259b5b8e0bb2be1aec7d7350c6
apple <apple@appleui-MacBookAir.local>
2023-12-02 21:36:38 +0500

    first commit

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,15 @@
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import {BuildOptions} from "../types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstance[] {
+    return [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({ template: options.paths.html }),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[contenthash:8].css",
+            chunkFilename: "css/chunk.[contenthash:8].css"
+        }),
+    ];
+}
